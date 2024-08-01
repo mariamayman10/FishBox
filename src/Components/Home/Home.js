400dac375f7eb268dd6df599e8466bf184246efa
mariamayman10 <mariamayman3131@gmail.com>
2024-08-01 21:00:25 +0300

    fix css conflicts + convert css to botstrap

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
-import HomeS1 from "./HomeS1";
-import HomeS2 from "./HomeS2";
-import HomeS3 from "./HomeS3";
-import HomeS4 from "./HomeS4";
-import HomeS5 from "./HomeS5";
-import HomeS6 from "./HomeS6";
+import HomeS1 from "./HomeSections/HomeS1";
+import HomeS2 from "./HomeSections/HomeS2";
+import HomeS3 from "./HomeSections/HomeS3";
+import HomeS4 from "./HomeSections/HomeS4";
+import HomeS5 from "./HomeSections/HomeS5";
+import HomeS6 from "./HomeSections/HomeS6";
 
 const Home = () => {
   return (
---
+++ src/Components/Home/HomeComponents/HomeC4.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../../CSS/Home.css";
+
+const HomeC4 = ({ img, p1, p2 }) => {
+  return (
+    <>
+        <div className="section4-item my-2">
+          <img src={img} alt="" className="section4-img mb-3" />
+          <div>
+            <p className="section4-type mb-1">{p1}</p>
+            <p className="section4-description">{p2}</p>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default HomeC4;
