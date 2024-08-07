f97e319ddfa8ca6f390e61bfa3e945878923e2e3
mariamayman10 <mariamayman3131@gmail.com>
2024-08-08 00:18:55 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import './App.css';
-import Home from './Components/Home/Home';
-import About from './Components/About/About';
-import MyAccount from './Components/MyAccount/MyAccount';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import About from "./Components/About/About";
+import MyAccount from "./Components/MyAccount/MyAccount";
+import Home from "./Components/Home/Home";
+import Shop from "./Components/Shop";
+import Recipes from "./Components/Recipes";
+import Blogs from "./Components/Blogs";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <>
-      {/* <Home /> */}
-    {/*<About/>*/}
-      <MyAccount />
-      {/* <Drawer/> */}
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/myaccount" element={<MyAccount />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
-}
+};
 
 export default App;
